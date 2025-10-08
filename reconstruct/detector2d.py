7ad76a0afc86f31356806231d179aba51ae21d1d
KouraniMEKA <kourani.ahmad@gmail.com>
2025-10-08 22:43:31 +0300

    Enable PyTorch V2 support.

--- reconstruct/detector2d.py
+++ reconstruct/detector2d.py
@@ -20,10 +20,27 @@ import cv2
 import torch
 import numpy as np
 import mmcv
-from mmcv.runner import load_checkpoint
-from mmdet.models import build_detector
-from mmdet.core import get_classes
-from mmdet.apis import inference_detector
+# << ak251007_PyTorch2
+# TODO: review and remove legacy MMLab V1 support after full testing is complete.
+from packaging.version import parse as parse_version
+
+# MMLab V1 vs V2 API compatibility check
+IS_MMCV_V2 = parse_version(mmcv.__version__) >= parse_version('2.0.0')
+
+if IS_MMCV_V2:
+    from mmengine.runner import load_checkpoint
+    from mmdet.registry import MODELS
+    from mmengine import Config  
+    # New imports for manual inference pipeline
+    from mmdet.utils import register_all_modules
+    from mmengine.dataset import Compose
+    from mmdet.apis import inference_detector as legacy_inference_detector # Keep old name for else block
+else:
+    from mmcv.runner import load_checkpoint
+    from mmdet.models import build_detector
+    from mmdet.core import get_classes
+    from mmdet.apis import inference_detector
+# ak251007_PyTorch2 >>
 
 
 object_class_table = {"cars": [2], "chairs": [56, 57]}
@@ -35,54 +52,164 @@ def get_detector2d(configs):
 
 class Detector2D(object):
     def __init__(self, configs):
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
         config = configs.Detector2D.config_path
         checkpoint = configs.Detector2D.weight_path
         if isinstance(config, str):
-            config = mmcv.Config.fromfile(config)
-        elif not isinstance(config, mmcv.Config):
+        # << ak251007_PyTorch2
+            if IS_MMCV_V2:
+                config = Config.fromfile(config)
+            else:
+                config = mmcv.Config.fromfile(config)
+
+        elif not isinstance(config, (mmcv.Config, Config if IS_MMCV_V2 else mmcv.Config)):
+        # ak251007_PyTorch2 >>
             raise TypeError('config must be a filename or Config object, '
                             f'but got {type(config)}')
-        config.model.pretrained = None
-        config.model.train_cfg = None
-        self.model = build_detector(config.model, test_cfg=config.get('test_cfg'))
-        if checkpoint is not None:
-            checkpoint = load_checkpoint(self.model, checkpoint, map_location='cpu')
-            if 'CLASSES' in checkpoint.get('meta', {}):
-                self.model.CLASSES = checkpoint['meta']['CLASSES']
-            else:
-                warnings.simplefilter('once')
-                warnings.warn('Class names are not saved in the checkpoint\'s '
-                              'meta data, use COCO classes by default.')
-                self.model.CLASSES = get_classes('coco')
+        
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 support after full testing is complete.
+        if IS_MMCV_V2:
+            # This function will automatically find and register all MMDetection components
+            # (models, backbones, etc.) into the MMEngine registry.      
+            from mmdet.utils import register_all_modules
+            register_all_modules()
+
+            # MMLab V2+ models do not accept 'pretrained' argument.
+            # We must remove it from the config dictionary before building the model.
+            if 'pretrained' in config.model:
+                del config.model['pretrained']
+            config.model.train_cfg = None
+
+            # Manually replicate the legacy initialization steps using the new API
+            # 1. Build the model
+            self.model = MODELS.build(config.model)
+
+            # 2. Load the checkpoint
+            if checkpoint is not None:
+                load_checkpoint(self.model, checkpoint, map_location='cpu')
+            
+            # In MMDetection v3.x, the data pipeline is defined in test_dataloader.
+            # The first step 'LoadImageFromFile' is for loading image from a path.
+            # Since we are passing an already-loaded image (np.ndarray), we replace
+            # it with a dummy transform that does nothing but ensures the pipeline
+            # starts correctly.
+            test_pipeline = config.test_dataloader.dataset.pipeline
+            # Replace 'LoadImageFromFile' with a dummy identity transform
+            if test_pipeline[0]['type'] == 'LoadImageFromFile':
+                test_pipeline = test_pipeline[1:]
+            self.pipeline = Compose(test_pipeline)
+
+        else:
+            # Legacy MMLab V1 requires these keys to be explicitly set.
+            config.model.pretrained = None
+            config.model.train_cfg = None
+            
+            self.model = build_detector(config.model, test_cfg=config.get('test_cfg'))
+
+            if checkpoint is not None:
+                checkpoint_data = load_checkpoint(self.model, checkpoint, map_location='cpu')
+                if 'CLASSES' in checkpoint_data.get('meta', {}):
+                    self.model.CLASSES = checkpoint_data['meta']['CLASSES']
+                else:
+                    warnings.simplefilter('once')
+                    warnings.warn('Class names are not saved in the checkpoint\'s '
+                                'meta data, use COCO classes by default.')
+                    self.model.CLASSES = get_classes('coco')
+
         self.model.cfg = config  # save the config in the model for convenience
-        self.model.to(device)
+        # 3. Move model to the correct device
+        # self.model.to(device)
+        self.model.to(self.device)
+        # ak251007_PyTorch2 >>
+
         self.model.eval()
         self.min_bb_area = configs.min_bb_area
         self.predictions = None
 
     def make_prediction(self, image, object_class="cars"):
         assert object_class == "chairs" or object_class == "cars"
-        self.predictions = inference_detector(self.model, image)
-        boxes = [self.predictions[0][o] for o in object_class_table[object_class]]
-        boxes = np.concatenate(boxes, axis=0)
-        masks = []
-        n_det = 0
-        for o in object_class_table[object_class]:
-            masks += self.predictions[1][o]
-            n_det += len(self.predictions[1][o])
+        
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 support after full testing is complete.
+        if IS_MMCV_V2:
+            # 1. Prepare data for the pipeline
+            data = dict(img=image, img_id=0, img_shape=image.shape[:2], ori_shape=image.shape[:2])
 
-        # In case there is no detections
-        if n_det == 0:
-            masks = np.zeros((0, 0, 0))
+            # 2. Run the pipeline
+            data = self.pipeline(data)
+            
+            # 3. Collate data into a batch
+            from mmengine.dataset import pseudo_collate
+            data = pseudo_collate([data])
+            data['inputs'][0] = data['inputs'][0].to(self.device)
+            data['data_samples'][0] = data['data_samples'][0].to(self.device)
+                      
+            # 4. Perform inference
+            with torch.no_grad():
+                results_list = self.model.test_step(data)
+            
+            # 5. Parse results from the DetDataSample object
+            results = results_list[0]
+            self.predictions = results
+            pred_instances = results.pred_instances
+            all_boxes = pred_instances.bboxes
+            all_scores = pred_instances.scores
+            all_labels = pred_instances.labels
+            all_masks = pred_instances.masks.cpu().numpy() if 'masks' in pred_instances else np.zeros((0, *image.shape[:2]))
+
+            boxes_list = []
+            masks_list = []
+            for class_id in object_class_table[object_class]:
+                class_mask_filter = (all_labels == class_id)
+                class_boxes = all_boxes[class_mask_filter]
+                class_scores = all_scores[class_mask_filter]
+                
+                boxes_with_scores = torch.cat([class_boxes, class_scores.unsqueeze(1)], dim=1)
+                
+                boxes_list.append(boxes_with_scores.cpu().numpy())
+                masks_list.append(all_masks[class_mask_filter.cpu().numpy()])
+
+            if not boxes_list:
+                boxes = np.zeros((0, 5))
+                masks = np.zeros((0, 0, 0))
+            else:
+                boxes = np.concatenate(boxes_list, axis=0)
+                masks = np.concatenate(masks_list, axis=0)
+
         else:
-            masks = np.stack(masks, axis=0)
-        assert boxes.shape[0] == masks.shape[0]
+            # Legacy MMLab V1 returns a tuple of (boxes, masks)
+            self.predictions = inference_detector(self.model, image)
+            boxes = [self.predictions[0][o] for o in object_class_table[object_class]]
+            boxes = np.concatenate(boxes, axis=0)
+            masks = []
+            n_det = 0
+            for o in object_class_table[object_class]:
+                masks += self.predictions[1][o]
+                n_det += len(self.predictions[1][o])
 
+            # In case there are no detections
+            if n_det == 0:
+                masks = np.zeros((0, 0, 0))
+            else:
+                masks = np.stack(masks, axis=0)
+        # ak251007_PyTorch2 >>
+
+        assert boxes.shape[0] == masks.shape[0]
         return self.get_valid_detections(boxes, masks)
 
     def visualize_result(self, image, filename):
-        self.model.show_result(image, self.predictions, out_file=filename)
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 support after full testing is complete.
+        if IS_MMCV_V2:
+            # The new API uses a different method signature
+            self.model.show_result(image, self.predictions, out_file=filename)
+        else:
+            # Legacy MMLab V1 show_result method
+            self.model.show_result(image, self.predictions, out_file=filename)
+        # ak251007_PyTorch2 >>
 
     def get_valid_detections(self, boxes, masks):
         # Remove those on the margin
--- reconstruct/detector3d.py
+++ reconstruct/detector3d.py
@@ -17,9 +17,24 @@
 
 import torch
 import mmcv
-from mmcv.runner import load_checkpoint
-from mmdet3d.models import build_model
-from mmdet3d.apis import inference_detector, convert_SyncBN
+# << ak251007_PyTorch2
+# TODO: review and remove legacy MMLab V1 API support after full testing is complete.
+from packaging.version import parse as parse_version
+
+# MMLab V1 vs V2 API compatibility check
+IS_MMCV_V2 = parse_version(mmcv.__version__) >= parse_version('2.0.0')
+
+if IS_MMCV_V2:
+    from mmengine.runner import load_checkpoint
+    from mmdet3d.apis import init_model, inference_detector
+    from mmengine.model import revert_sync_batchnorm
+    from mmengine.registry import MODELS
+    from mmengine import Config  
+else:
+    from mmcv.runner import load_checkpoint
+    from mmdet3d.models import build_model
+    from mmdet3d.apis import inference_detector, convert_SyncBN
+# ak251007_PyTorch2 >>
 
 
 def get_detector3d(configs):
@@ -34,35 +49,107 @@ class Detector3D(object):
         checkpoint = configs.Detector3D.weight_path
 
         if isinstance(config, str):
-            config = mmcv.Config.fromfile(config)
-        elif not isinstance(config, mmcv.Config):
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 API support after full testing is complete.
+            if IS_MMCV_V2:
+                config = Config.fromfile(config)
+            else:
+                config = mmcv.Config.fromfile(config)
+
+        elif not isinstance(config, (mmcv.Config, Config if IS_MMCV_V2 else mmcv.Config)):
+        # ak251007_PyTorch2 >>
             raise TypeError('config must be a filename or Config object, '
                             f'but got {type(config)}')
 
-        config.model.pretrained = None
-        convert_SyncBN(config.model)
+        
         config.model.train_cfg = None
-        self.model = build_model(config.model, test_cfg=config.get('test_cfg'))
 
-        if checkpoint is not None:
-            checkpoint = load_checkpoint(self.model, checkpoint, map_location='cpu')
-            if 'CLASSES' in checkpoint['meta']:
-                self.model.CLASSES = checkpoint['meta']['CLASSES']
-            else:
-                self.model.CLASSES = config.class_names
-            if 'PALETTE' in checkpoint['meta']:  # 3D Segmentor
-                self.model.PALETTE = checkpoint['meta']['PALETTE']
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 API support after full testing is complete.
+        if IS_MMCV_V2:
+            # This function will automatically find and register all MMDetection3D components
+            # (models, backbones, etc.) into the MMEngine registry.      
+            # The 3D models often depend on components (like loss functions) from the 2D library.
+            # We must register both.
+            from mmdet3d.utils import register_all_modules as register_all_modules_mmdet3d
+            from mmdet.utils import register_all_modules as register_all_modules_mmdet
+            
+            # We establish 'mmdet3d' as the primary scope,
+            # then add the 'mmdet' components without overwriting that scope.
+            register_all_modules_mmdet3d(init_default_scope=True)
+            register_all_modules_mmdet(init_default_scope=False)
+
+            # The VoxelNet model uses 'FocalLoss', which is defined in `mmdet`.
+            # We must explicitly add `_scope_='mmdet'` to the loss configuration
+            # to tell the builder where to find this cross-library component.
+            if 'bbox_head' in config.model:
+                if 'loss_cls' in config.model.bbox_head:
+                    config.model.bbox_head.loss_cls['_scope_'] = 'mmdet'
+                if 'loss_bbox' in config.model.bbox_head: 
+                    config.model.bbox_head.loss_bbox['_scope_'] = 'mmdet'
+                if 'loss_dir' in config.model.bbox_head: 
+                    config.model.bbox_head.loss_dir['_scope_'] = 'mmdet'
+
+            # MMLab V2+ models have different constructor arguments.
+            # We must remove the old, incompatible keys from the config dictionary.
+            if 'pretrained' in config.model:
+                del config.model['pretrained']
+            if 'voxel_layer' in config.model: 
+                del config.model['voxel_layer']
+
+            # Manually replicate the legacy initialization steps using the new API
+            # 1. Build the model
+            self.model = MODELS.build(config.model)
+
+            # 2. Load the checkpoint
+            if checkpoint is not None:
+                # The new load_checkpoint is from mmengine
+                load_checkpoint(self.model, checkpoint, map_location='cpu', revise_keys=[(r'^pts_', '')])
+
+        else:
+            # Legacy MMLab V1 requires these keys to be explicitly set.
+            config.model.pretrained = None
+            convert_SyncBN(config.model)
+            config.model.train_cfg = None
+            
+            self.model = build_model(config.model, test_cfg=config.get('test_cfg'))
+            
+            if checkpoint is not None:
+                checkpoint = load_checkpoint(self.model, checkpoint, map_location='cpu')
+                if 'CLASSES' in checkpoint['meta']:
+                    self.model.CLASSES = checkpoint['meta']['CLASSES']
+                else:
+                    self.model.CLASSES = config.class_names
+                if 'PALETTE' in checkpoint['meta']:  # 3D Segmentor
+                    self.model.PALETTE = checkpoint['meta']['PALETTE']
+
         self.model.cfg = config  # save the config in the model for convenience
+        # 3. Move model to the correct device
         self.model.to(device)
         self.model.eval()
+        # ak251007_PyTorch2 >>
+
 
     def make_prediction(self, velo_file):
-        predictions, data = inference_detector(self.model, velo_file)
+        # << ak251007_PyTorch2
+        # TODO: review and remove legacy MMLab V1 API support after full testing is complete.
+        if IS_MMCV_V2:
+            # MMLab V2+ returns a structured Det3DDataSample object
+            results = inference_detector(self.model, velo_file)
+            pred_instances = results[0].pred_instances_3d
+
+            # Extract data from the new structured object
+            labels = pred_instances.labels_3d
+            scores = pred_instances.scores_3d
+            boxes = pred_instances.bboxes_3d.tensor
+        else:
+            # Legacy MMLab V1 returns a tuple of (predictions, data)
+            predictions, data = inference_detector(self.model, velo_file)
+            labels = predictions[0]["labels_3d"]
+            scores = predictions[0]["scores_3d"]
+            boxes = predictions[0]["boxes_3d"].tensor
+        # ak251007_PyTorch2 >>
+
         # Car's label is 0 in KITTI dataset
-        labels = predictions[0]["labels_3d"]
-        scores = predictions[0]["scores_3d"]
         valid_mask = (labels == 0) & (scores > 0.0)
-        boxes = predictions[0]["boxes_3d"].tensor
-
         return boxes[valid_mask]
-
